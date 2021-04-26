18d4c4d1b8f23f3700d65a3e206efa12885ffedb
Adri <apereg24@estudiantes.unileon.es>
2021-04-26 20:38:53 +0200

    Funcionalidad justa para pasar el dia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const dictionary = []
-const numberOfEntries = 60
 
 function launch() {
     loadDictionary()
@@ -8,14 +7,11 @@ function launch() {
 }
 
 function loadDictionary() {
-    let word
     /* Se realiza la peticion asincrona con fetch. */
     fetch("https://ordenalfabetix.unileon.es/aw/diccionario.txt").then(r => r.text()).then(function (dic) {
         /* Se separa el String recibido por saltos de linea metiendo las palabras a una lista. */
         while (dic.indexOf("\n") !== -1) {
-            word = dic.substr(0, dic.indexOf("\n"))
-            if (word.length == 4 || word.length == 6)
-                dictionary.push(word)
+            dictionary.push(dic.substr(0, dic.indexOf("\n")).toUpperCase())
             dic = dic.substr(dic.indexOf("\n") + 1)
         }
     })
@@ -28,7 +24,7 @@ function createTable() {
     for (let i = 0; i < 6; i++) {
         table += "<tr>"
         for (let j = 0; j < 4; j++)
-            table += "<td id=\"box" + (++id) + "\" class=\"box\" maxLength=\"1\"><input type=\"text\" class=\"box\" onKeyUp=checkBoardFour()></td>"
+            table += "<td id='box" + (++id) + "' class='box'><input type=\"text\" maxLength='1' class=\"box\" onKeyUp=checkBoardFour()></td>"
         table += "</tr>"
     }
     document.getElementById("tableFour").innerHTML = table + "</table"
@@ -43,65 +39,102 @@ function createTable() {
     document.getElementById("tableSix").innerHTML = table + "</table"
 }
 
-function markCorrect(cells) {
-    console.log("aaaaaaaaaaaaaaaaaa")
-    for (let j = 0; j < cells.length; j++) {
-        cells[j].classList.add('boxCorrect')
-        cells[j].classList.remove('box')
-    }
+async function markCorrect(cells) {
+    console.log("aaaaaaaaaaaaaaaaaaaaaa")
+    /*queueMicrotask(() => {
+        for (let i = 0; i < 100; i++) {
+            cells.classList.add('boxCorrect')
+            cells.classList.remove('box')
+            sleep(100)
+            cells.classList.remove('boxCorrect')
+            cells.classList.add('box')
+        }
+    });*/
 }
 
-function markFalse(cells) {
+function markFalse() {
     console.log("bbbbbbbbbbbbbbbbbbb")
 }
 
 function checkBoardFour() {
-    const table = document.getElementById("tableFour");
+    console.log("----------------------")
+    console.log("Se comprueba el tablero de cuatro")
+    const table = document.getElementById("tableFour")
     for (let i = 0; i < table.rows.length; i++) {
         let word = mapWord(table.rows[i].getElementsByTagName('td'))
-        if(word.length == 4){
-            if(i == 0){
-                if(word == "CLAN")
-                    markCorrect(table.rows[i].getElementsByTagName('td'))
-                else
-                    markFalse(table.rows[i].getElementsByTagName('td'))
+        console.log("Se lee la palabra " + word + " en la posicion " + i)
+        if (word.length === 4) {
+            if (dictionary.includes(word)) {
+                console.log("Ready")
+                if (i === 0) {
+                    ((word === "CLAN") ? markCorrect(table.rows[i].getElementsByTagName('td')) : markFalse(table.rows[i].getElementsByTagName('td')))
+                } else if (i === table.rows.length - 1) {
+                    ((word === "PENA") ? markCorrect(table.rows[i].getElementsByTagName('td')) : markFalse(table.rows[i].getElementsByTagName('td')))
+                } else {
+                    console.log("Se va a comparar")
+                    let lastWord = mapWord(table.rows[i - 1].getElementsByTagName('td'))
+                    if (dictionary.includes(lastWord)) {
+                        if (!checkVeracity(word, lastWord, i)) {
+                            alert("Palabra incorrecta: " + word + " (No cumple las condiciones respecto a " + lastWord + ")")
+                            deleteLastChar(table.rows[i].getElementsByTagName('td'))
+                        }
+                    } else {
+                        alert("Complete antes la palabra anterior")
+                    }
+                }
+            } else{
+                alert("La palabra " +word.toLowerCase()+ " no esta registrada en el diccionario")
+                deleteLastChar(table.rows[i].getElementsByTagName('td'))
             }
-
+        } else {
+            console.log("Aun no esta ready")
         }
-        console.log(word)
     }
 }
 
 function checkBoardSix() {
 }
 
-function mapWord(cells){
+function deleteLastChar(word){
+    word[word.length - 1].getElementsByTagName('input')[0].value = ""
+}
+
+function mapWord(cells) {
     let word = ""
     for (let j = 0; j < cells.length; j++) {
-        const cellValue = cells[j].getElementsByTagName("input")[0].value;
-        if(cellValue.length > 1)
-            cells[j].getElementsByTagName("input")[0].value = cellValue.substr(cellValue.length-1)
         word += cells[j].getElementsByTagName("input")[0].value
     }
     return word.toUpperCase()
 }
 
-function checkVeracity(word1, word2, index){
-    return ((index != 1)?checkCross(word1, word2):checkChange(word1, word2))
+function checkVeracity(word1, word2, index) {
+    console.log(index)
+    return ((index % 2 === 0) ? checkCross(word1, word2) : checkChange(word1, word2))
 }
 
 function checkCross(word1, word2) {
-    return getCoincidences(word1, word2) != word1.length;
+    console.log("Se llega el check cross con " + word1 + " y " + word2)
+    let coincidences = 0
+    for (let i = 0; i < word1.length; i++)
+        if (word1.includes(word2[i]))
+            coincidences++
+    return coincidences === word1.length;
 }
 
 function checkChange(word1, word2) {
-    return getCoincidences(word1, word2) != word1.length-1;
-}
-
-function getCoincidences(word1, word2) {
+    console.log("Se llega el check change con " + word1 + " y " + word2)
     let coincidences = 0
     for (let i = 0; i < word1.length; i++)
-        if(word1[i] == word2[i])
+        if (word1[i] == word2[i])
             coincidences++
-    return coincidences
+    return coincidences === word1.length - 1;
+}
+
+function sleep(milliseconds) {
+    const start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
 }
